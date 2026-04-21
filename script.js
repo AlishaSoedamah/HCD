8ed554b2075e0c0ee1508db51dbb7bf25348bb55
Alisha Soedamah <alisha.soedamah@gmail.com>
2026-04-21 13:14:35 +0200

    boe

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function playtwo() {
     var audiotwo = document.getElementById("audiotwo");
     audiotwo.play();
 }
+
+const input = document.getElementById('searchbar');
+const dropdown = input.nextElementSibling;
+
+input.addEventListener('focus', () => dropdown.style.opacity = '1');
+document.addEventListener('click', (e) => {
+  if (!input.contains(e.target) && !dropdown.contains(e.target)) {
+    dropdown.style.opacity = '0';
+  }
+});
